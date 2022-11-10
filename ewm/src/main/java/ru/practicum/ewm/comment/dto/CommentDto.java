dcd26f3655864cb45ff127826dcb4d4afd8254ef
Sash <notsadako@yandex.ru>
2022-11-10 13:03:24 +0300

    comments feature fix

--- ewm/src/main/java/ru/practicum/ewm/comment/dto/CommentDto.java
+++ ewm/src/main/java/ru/practicum/ewm/comment/dto/CommentDto.java
@@ -8,8 +8,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CommentDto {
-    long id;
-    String text;
-    long userId;
-    long eventId;
+    private long id;
+    private String text;
+    private long userId;
+    private long eventId;
 }
--- ewm/src/main/java/ru/practicum/ewm/comment/dto/NewCommentDto.java
+++ ewm/src/main/java/ru/practicum/ewm/comment/dto/NewCommentDto.java
@@ -8,5 +8,5 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class NewCommentDto {
-    String text;
+    private String text;
 }
